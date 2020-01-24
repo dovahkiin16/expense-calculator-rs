b0109845bfa0edba30adaa53a765b31516f78314
dovahkiin16 <edenfernandezjr@gmail.com>
2020-01-24 11:55:12 +0800

    fix deployment issue

--- push_image.sh
+++ push_image.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 REPO="docker.pkg.github.com/dovahkiin16"
-TAG=$(git describe)
+DOCKER_TAG=$(git describe)
+export DOCKER_TAG
 
-echo "TAG $TAG"
+echo "TAG $DOCKER_TAG"
 
 # login docker
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin docker.pkg.github.com
 
 # build
 docker build -t $REPO/expense_calculator:latest \
-  -t $REPO/expense-calculator:"$TAG" .
+  -t $REPO/expense-calculator:"$DOCKER_TAG" .
 
 # push docker image
 docker push $REPO/expense_calculator:latest
-docker push $REPO/expense_calculator:"$TAG"
+docker push $REPO/expense_calculator:"$DOCKER_TAG"
