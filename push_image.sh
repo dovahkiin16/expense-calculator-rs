4bd23ea9073bcdd74315d7579f2dfebd3ad3527b
dovahkiin16 <edenfernandezjr@gmail.com>
2020-01-24 23:37:28 +0800

    fix postgres error on deployment

--- push_image.sh
+++ push_image.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-REPO="docker.pkg.github.com/dovahkiin16"
-echo "TAG $DOCKER_TAG"
-
 # login docker
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin docker.pkg.github.com
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin $DOCKER_HOST
 
 # build
-docker build -t $REPO/expense_calculator:latest \
-  -t $REPO/expense-calculator:"$DOCKER_TAG" .
-
-# push docker image
-docker push $REPO/expense_calculator:latest
-docker push $REPO/expense_calculator:"$DOCKER_TAG"
+docker build -t $DOCKER_REPO/expense_calculator:latest \
+  -t $DOCKER_REPO/expense-calculator:"$DOCKER_TAG" .
